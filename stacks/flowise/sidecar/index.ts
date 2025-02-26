9a34ce1e5ea9506f17af03d03f5917db6eac8da3
Feodor Rusanov <f.rusanov@gmail.com>
2025-02-26 19:31:49 +0300

    WIP

--- stacks/flowise/sidecar/index.ts
+++ stacks/flowise/sidecar/index.ts
@@ -1,11 +1,12 @@
 import { Hono } from "hono";
+import { serve } from "@hono/node-server";
 import { tools } from "./tools";
 
 const app = new Hono();
 
 app.route("/tools", tools);
 
-export default {
+serve({
   port: 3322,
   fetch: app.fetch,
-};
+});
