cc9ecdeee0e7125604f07d2ecf64cc2167b4a31f
Feodor Rusanov <f.rusanov@gmail.com>
2025-07-17 22:37:15 +0700

    feat: add n8n

--- stacks/flowise/sidecar/types/function.ts
+++
@@ -1,7 +0,0 @@
-import type { ZodSchema } from "zod";
-
-export interface FunctionTool<T = any, P = any> {
-  name: string;
-  fn: (params: P) => Promise<T>;
-  parameters: ZodSchema<P>;
-}
--- stacks/flowise/sidecar/utils/error-with-code.ts
+++
@@ -1,8 +0,0 @@
-export class ErrorWithCode extends Error {
-  code!: number;
-
-  constructor(message: string, options: { cause?: unknown; code: number }) {
-    super(message, options);
-    this.code = options.code;
-  }
-}
